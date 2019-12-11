372948edd80ad147c26a780ab62c763cde4c9a3a
Daryl Pinto <daryl.j.pinto@gmail.com>
2019-12-11 16:02:31 -0500

    Add ultimate stock icons

--- ultimate/src/components/CharacterPicker/CharacterPicker.js
+++ ultimate/src/components/CharacterPicker/CharacterPicker.js
@@ -4,15 +4,22 @@ import Modal from "../Modal";
 import characterData from "../../data/characters";
 
 const CharacterPicker = ({ value, onChange }) => {
-	const [modalOpen, setModalOpen] = useState(false);
+	const [modalOpen, setModalOpen] = useState(true);
 
 	return (
-		<div>	
+		<div>
 			<div className={s.selectionBox} onClick={() => setModalOpen(true)}>
 				{value}
 			</div>
+			<img src="images/stocks/ultimate/Yoshi.png" alt="" />
 			<Modal isOpen={modalOpen} close={() => setModalOpen(false)}>
-				{characterData.map(character => <p>{character}</p>)}
+				{characterData.map(character => (
+					<img
+						src={`images/stocks/ultimate/${character}.png`}
+						alt={character}
+						className={s.stockIcon}
+					/>
+				))}
 			</Modal>
 		</div>
 	);
