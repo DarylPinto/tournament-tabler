5756bd2d9fd37c058f5b6b281d5a0c4671fbefee
Daryl Pinto <daryl.j.pinto@gmail.com>
2020-02-02 23:19:17 -0500

    Begin working on home screen

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 import s from "./Home.module.scss";
 
 const Home = () => {
 	return (
-		<div className={s.links}>
-			<Link to="/smash64">Smash 64</Link>
-			<Link to="/melee">Melee</Link>
-			<Link to="/brawl">Brawl</Link>
-			<Link to="/projectm">Project M</Link>
-			<Link to="/smash4">Smash 4</Link>
-			<Link to="/ultimate">Ultimate</Link>
-		</div>
+		<main>
+			<a href="/" className={s.logo}>
+				<h1>Tournament Tabler</h1>
+			</a>
+			<section className={s.mainSection}>
+				<div className={s.links}>
+					<Link to="/smash64">Smash 64</Link>
+					<Link to="/melee">Melee</Link>
+					<Link to="/brawl">Brawl</Link>
+					<Link to="/projectm">Project M</Link>
+					<Link to="/smash4">Smash 4</Link>
+					<Link to="/ultimate">Ultimate</Link>
+				</div>
+			</section>
+		</main>
 	);
 };
 
